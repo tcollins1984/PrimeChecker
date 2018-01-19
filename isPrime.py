9d6490142536a0c2caa04524de70edc481528c64
tcollins1984 <thomas.anthony.collins.tc@gmail.com>
2018-01-19 14:24:46 -0500

    Create isPrime.py
    This function will check to see if any number greater than 3 is prime or composite. Even numbers are automatically labelled composite.  Odd numbers, which are the only candidates for primes greater than 2 are subjected to a test where all odd numbers up to about N/3 are checked as factors.  This is because an odd number will only have odd factors and the biggest factor it may have is N/3. This function actually overshoots N/3 by one more number to be sure all relevant possible factors are checked.

---
+++ isPrime.py
@@ -0,0 +1,45 @@
+def isPrime(x):
+#checks to see  whether a number greather than 3 is prime or composite    
+    x = int(x)
+    y = x+1
+    z = x+2
+    c = 0
+    if x < 4:
+        print("Please enter an integer greater than 3")
+
+    else:
+        if x%2 == 0:
+            c = c + 1
+
+        elif x%3 == 0:
+            c = c+1
+
+        elif((x+1)%3 == 0):
+            m = int((x+1)/3)
+            if m%2 == 0:
+                n = int(m/2)
+                for i in range(1,n+1):
+                    if x%(2*i+1) == 0:
+                        c = c + 1
+            elif m%2 != 0:
+                n = int((m+1)/2)
+                for i in range(1,n+1):
+                    if x%(2*i+1) == 0:
+                        c = c + 1
+        elif((x+2)%3 == 0):
+            m = int((x+2)/3)
+            if m%2 == 0:
+                n = int(m/2)
+                for i in range(1,n+1):
+                    if x%(2*i+1) == 0:
+                        c = c + 1
+            elif m%2 != 0:
+                n = int((m+1)/2)
+                for i in range(1,n+1):
+                    if x%(2*i+1) == 0:
+                        c = c + 1
+
+        if c == 0:
+            return('Prime')
+        else:
+            return('Composite')
